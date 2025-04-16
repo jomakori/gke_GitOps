7eea19fd5332b2bf6f4970df61211424b066fb9f
Joel Makori <joe3rdwash@gmail.com>
2025-04-16 17:00:55 +0200

    style(script): use echo instead of printf for output messages

--- ct_check.sh
+++ ct_check.sh
@@ -15,7 +15,7 @@ RESET="\033[0m"
 # Determine helm directories from changed files
 for file in "${files_array[@]}"; do
   if [[ "$file" == *"/templates/"* || "$file" == *"/helm/"* ]]; then
-    ## Extract the directory path up to the level above 'templates' or 'helm'
+    ## Grab helm path from file path
     helm_dir=$(dirname "$file" | sed 's|\(/templates\|/helm\).*||')
     ## Add to the helm_dirs array if not already present
     if [[ ! " ${helm_dirs[@]} " =~ " ${helm_dir} " ]]; then
@@ -26,24 +26,23 @@ done
 
 # Run chart testing on each Helm directory
 echo "::notice::Running chart testing on: ${helm_dirs[@]}"
-
 for dir in "${helm_dirs[@]}"; do
   ## Test apps/services
   if [[ "$dir" == *"helm"* ]]; then
-    printf "${GREEN}Running ct lint-and-install on: ${RESET}" "$dir"
+    echo -e "${GREEN}Running ct lint-and-install on: ${RESET} $dir"
     ct lint-and-install --charts "$dir" --validate-maintainers=false
   
   ## Lint argocd templates
   else
-    printf "${BLUE}Running ct lint on: ${RESET}" "$dir"
+    echo -e "${BLUE}Running ct lint on: ${RESET} $dir"
     ct lint --charts "$dir" --validate-maintainers=false
   fi
 
   ## Fail-catch
   if [ $? -ne 0 ]; then
-    printf "${RED}ERROR: ct command failed on: ${RESET}" "$dir"
-    exit 1
+    echo -e "${RED}::ERROR:: ct command failed on: ${RESET} $dir"
   fi
 done
 
+# Confirm success
 echo "✅ Lint and Tested ${#helm_dirs[@]} changed Helm charts."
